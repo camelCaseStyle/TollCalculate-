00bb38df99d2cd104e451011832d8940af613b81
Anubhav <anubhav.ashish9@gmail.com>
2021-06-29 22:54:22 +1000

    have to expose api key

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Get the geocode for a given location
 app.post('/geoLocation', (request, response)=>{
-    axios.get(Constants.geoCodeAPIURL+`key=${process.env.API_KEY_GEOLOCATE}&address=${request.body.address}`)
+    axios.get(Constants.geoCodeAPIURL+`key=AIzaSyALUiYReYJjr6VgTxzfYgw7IDeMFX7KU-w&address=${request.body.address}`)
     .then(function(res){
         response.send(res.data)
     })
@@ -26,7 +26,7 @@ app.post('/tollCalculate', (request, response)=>{
     axios.post(Constants.tollRequesURL,request.body,{
         headers:{
             'Content-Type':'application/json',
-            Authorization: `apikey ${process.env.API_KEY_SERVICENSW}`
+            Authorization: `apikey pPPPcmFo0knYMcgKnY68PTeI6lOFi8LBWn5C`
         }
     }).then(res =>{
         response.send(res.data);
